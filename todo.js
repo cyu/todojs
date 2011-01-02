c7bcb56dc86971f34a0da711dfaacc82f8224571
Calvin Yu <csyu77@gmail.com>
2011-01-01 23:36:31 -0500

    reworked UI

--- todo.js
+++ todo.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
-    $('#email').bind('focus', function(){ $(this).select(); });
-    $('#new-task').bind('focus', function(){ $(this).select(); });
-    $('#new-task-button').click(function(){
-        var tasks = window.appStorage.tasks || [];
+    var showNewTaskForm = function() {
+        var divs = $('#add').children('div');
+        if (!divs.first().is(':hidden')) {
+            $(divs.first()).hide();
+            $(divs.get(1)).show().find('#new-task').focus();
+        }
+    };
+
+    var hideNewTaskForm = function() {
+        var divs = $('#add div');
+        if (divs.first().is(':hidden')) {
+            $(divs.get(1)).hide();
+            $(divs.first()).show();
+        }
+    };
+
+    $('#add').click(showNewTaskForm);
+
+    $('#add form').submit(function(){
         var newTask = $('#new-task').val();
-        tasks.push(newTask);
+        $('#new-task').val('');
+        var tasks = window.appStorage.tasks;
+        tasks.push({text:newTask});
         window.appStorage.setItem('tasks', tasks);
-        $('#task-list').append("<li class='task'>" + newTask + "</li>")
+        $('#task-list ol li#add').before("<li class=task>" + newTask + "</li>")
+        return false;
     });
+
+    $('#new-task').
+        keydown(function(event){
+            if (event.keyCode == 27) {
+                hideNewTaskForm();
+            }
+        });
 
     $('.task').live('click', function(){
         $(this).addClass('completed');
@@ -22,31 +47,36 @@ $(document).ready(function(){
         $(clicked).delay(1000).slideUp('slow');
     });
 
-    $('#twitter-login').click(function(){
+    $('#twitter-login a').click(function(){
         window.appStorage.$.connect({twitter:true});
     });
 
-    $('#login-button').click(function(){
+    $('#login form').submit(function(){
+        $('#login-button').attr('disabled', true);
         window.appStorage.$.connect({email: $('#email').val()});
-        $('#login').dialog('close');
-        $('#new-task-pane').show();
+        return false;
     });
 
-    var title = $('h1');
-    var titlePos = title.position();
-    $('#login').dialog({
-        resizable: false,
-        height: 'auto',
-        minHeight: 80,
-        position: [titlePos.left, titlePos.top + title.height()] });
+    $('#login').show();
 
     window.appStorage.$.ready(function(){
-        var tasks = window.appStorage.tasks || [];
-        for (var i=0; i<tasks.length; i++) {
-            $('#task-list').append("<li class='task'>" + tasks[i] + "</li>")
+        var tasks = window.appStorage.tasks;
+
+        if (!tasks) {
+            tasks = [
+                {text: 'This is an example todo task'},
+                {text: 'Click on a task to mark it completed'}
+            ];
+            window.appStorage.setItem('tasks', tasks);
         }
-        $('#login').dialog('close');
-        $('#new-task-pane').show();
+
+        for (var i=0; i<tasks.length; i++) {
+            $('#task-list li#add').before("<li class=task>" + tasks[i].text + "</li>")
+        }
+
+        $('#login').hide();
+        $('#task-list').show();
+        $(document).keypress(showNewTaskForm);
     });
 
 });
